ead19c2593ba06e4f62843569c46bd0ddf1fff08
hashfaq839 <hashfaq839@gmail.com>
2026-09-02 13:18:44 +0530

    Made some changes and added functionality

--- myfirstproject/src/com/javaintro/Addition.java
+++ myfirstproject/src/com/javaintro/Addition.java
@@ -7,7 +7,7 @@ public class Addition {
 	void add()	{
 		sum = a + b;
     }
-	void subraction(){
+	void subtraction(){
 		sub = a - b;
 	}
 	public static void main(String[] args) {
@@ -16,9 +16,9 @@ public class Addition {
         a1.a = 5;
         a1.b = 12;
         a1.add();
-        
+        a1.subtraction();
         System.out.println(" Sum of "+a1.a+" and "+a1.b+" is: " +a1.sum);
-        System.out.println(" Subraction of "+a1.a+" and "+a1.b+" is: " +a1.subraction);
+        System.out.println(" Subraction of "+a1.a+" and "+a1.b+" is: " +a1.subtraction);
 
         
 	}
