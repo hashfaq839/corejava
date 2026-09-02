919cbf35f791292a9557655c2184235d29013cfc
hashfaq839 <hashfaq839@gmail.com>
2026-09-02 13:16:19 +0530

    add subraction functionality

--- myfirstproject/src/com/javaintro/Addition.java
+++ myfirstproject/src/com/javaintro/Addition.java
@@ -2,12 +2,14 @@ package com.javaintro;
 
 public class Addition {
 
-	int a,b,sum;
+	int a,b,sum,sub;
     
 	void add()	{
 		sum = a + b;
     }
-	
+	void subraction(){
+		sub = a - b;
+	}
 	public static void main(String[] args) {
 
         Addition a1 = new Addition();
@@ -16,7 +18,8 @@ public class Addition {
         a1.add();
         
         System.out.println(" Sum of "+a1.a+" and "+a1.b+" is: " +a1.sum);
-        
+        System.out.println(" Subraction of "+a1.a+" and "+a1.b+" is: " +a1.subraction);
+
         
 	}
 
